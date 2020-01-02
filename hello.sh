323893758ac6e6dcf8720860593987dc2392000f
mangeshgajbhiye <49239714+mangeshgajbhiye@users.noreply.github.com>
2020-01-02 21:00:42 +0530

    this

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Hello World" # also you can write comment in the simple formate
+
+# this is comment section
+
+echo our shell name is $BASH
+echo Home directory is $HOME
+echo curent working directory $PWD
+echo version : $BASH_VERSION
+
+name=Mangesh
+val=10
+echo my name is $name
+echo value = $val
